6ada0a32cb41dd936ea0b51d0cad5b5e5b6a1644
NathanDubernat <77488680+NathanDubernat@users.noreply.github.com>
2021-03-31 09:32:51 +0200

    final version

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
-var num = 10000;
-var noiseScale=1000, noiseStrength=100;
+let img;
+function preload() {
+  img = loadImage('couleurs.jpeg');
+}
+
+var num = 3000;
+var noiseScale=500, noiseStrength=100;
 var particles = [num];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
+  pixelDensity(4);
   for (let i=0; i<num; i++) {
     var loc = createVector(random(width*1.2), random(height), 2);
-    var angle = 0;
+    var angle = 0; //any value to initialize
     var dir = createVector(cos(angle), sin(angle));
     var speed = random(0.5,2);
     particles[i]= new Particle(loc, dir, speed);
@@ -18,9 +24,7 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 function draw() {
-  fill(0, 10);
   noStroke();
-  circle(width/2, height/2, height);
   for (let i=0; i<particles.length; i++) {
     particles[i].run();
   }
@@ -34,7 +38,6 @@ class Particle{
   }
   run() {
     this.move();
-    this.checkEdges();
     this.update();
   }
   move(){
@@ -42,19 +45,14 @@ class Particle{
     this.dir.x = cos(angle);
     this.dir.y = sin(angle);
     var vel = this.dir.copy();
-    var d =1; 
-    vel.mult(this.speed*d);
-    this.loc.add(vel);
+    var d =1;  //direction change 
+    vel.mult(this.speed*d); //vel = vel * (speed*d)
+    this.loc.add(vel); //loc = loc + vel
   }
-  checkEdges(){
-    let distance = dist(width/2, height/2, this.loc.x, this.loc.y);
-    if (distance>width/2) {
-      this.loc.x = random(width*1.2);
-      this.loc.y = random(height);
-    }
-  }
-  update(){
-    fill(255);
-    ellipse(this.loc.x, this.loc.y, this.loc.z);
+ 
+  update() {
+    let p = img.get(this.loc.x, this.loc.y);
+      fill(p);
+      ellipse(this.loc.x, this.loc.y, this.loc.z);
   }
 }
