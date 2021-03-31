a3c769ad4e4808d326c0bed66c2fc19901273a14
NathanDubernat <77488680+NathanDubernat@users.noreply.github.com>
2021-03-31 17:11:14 +0200

    final

--- sketch V1.js
+++ sketch V1.js
@@ -1,15 +1,9 @@
-var num = 1000;
+var num = 10000;
 var noiseScale=500, noiseStrength=100;
 var particles = [num];
 
-let img;
-function preload() {
-  img = loadImage('couleurs.jpg');
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, 1);
   noStroke();
   for (let i=0; i<num; i++) {
     var loc = createVector(random(width*1.2), random(height), 2);
@@ -18,17 +12,15 @@ function setup() {
     var speed = random(0.5,2);
     particles[i]= new Particle(loc, dir, speed);
   }
-  
-    updatePixels(0, 0, width, height);
-    set(0, 0, img);
-    loadPixels();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 function draw() {
+  fill(0, 10);
   noStroke();
+  circle(width/2, height/2, height);
   for (let i=0; i<particles.length; i++) {
     particles[i].run();
   }
@@ -42,10 +34,11 @@ class Particle{
   }
   run() {
     this.move();
+    this.checkEdges();
     this.update();
   }
   move(){
-    let angle=noise(this.loc.x/noiseScale, this.loc.y/noiseScale, frameCount/noiseScale)*TWO_PI*noiseStrength; //0-2PI
+    let angle=noise(this.loc.x/noiseScale, this.loc.y/noiseScale, frameCount/noiseScale)*TWO_PI*noiseStrength;
     this.dir.x = cos(angle);
     this.dir.y = sin(angle);
     var vel = this.dir.copy();
@@ -53,9 +46,15 @@ class Particle{
     vel.mult(this.speed*d);
     this.loc.add(vel);
   }
- 
+  checkEdges(){
+    let distance = dist(width/2, height/2, this.loc.x, this.loc.y);
+    if (distance>width/2) {
+      this.loc.x = random(width*1.2);
+      this.loc.y = random(height);
+    }
+  }
   update(){
-    fil
+    fill(255);
     ellipse(this.loc.x, this.loc.y, this.loc.z);
   }
 }
